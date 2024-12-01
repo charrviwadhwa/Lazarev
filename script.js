01d7d4649f43be19b6e68b91d6f9f412bff928e8
Charvi Wadhwa <154724710+charrviwadhwa@users.noreply.github.com>
2024-12-02 03:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var nav = document.querySelector("nav")
+nav.addEventListener("mouseenter",()=>{
+    let tl = gsap.timeline()
+
+    tl.to("#nav_bottom",{
+        height:"25vh",
+       
+    })
+    tl.to(".nav1 h5",{
+        display:"block",
+        
+    })
+    tl.to(".nav1 h5 span",{
+        y:0,
+        stagger:{
+            amount:0.6
+        }
+    })
+})
+nav.addEventListener("mouseleave",()=>{
+    let tl = gsap.timeline()
+
+    tl.to(".nav1 h5 span",{
+        y:25,
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to(".nav1 h5",{
+       display:none,
+       duration:0.1
+    })
+    tl.to("#nav_bottom",{
+       height:0,
+       duration:0.1
+    })
+})
